e34b5f9c4cab8d3937c29923536085dbefbdc16b
nasirisara <99367926+nasirisara@users.noreply.github.com>
2022-04-13 23:07:40 +0200

    uppdate

--- MVCBasicsAssignment/Controllers/HomeController.cs
+++ MVCBasicsAssignment/Controllers/HomeController.cs
@@ -12,5 +12,17 @@ namespace MVCBasicsAssignment.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
     }
 }
